f7c77b3e9d4a952214f405458bd38c6fbf77c13f
mondoman712 <oommmmmmmo@googlemail.com>
2015-09-24 21:32:57 +0100

    removed old circle code

--- main.c
+++ main.c
@@ -1,53 +1,5 @@
 #include "defs.h"
 
-void set_pixel (SDL_Surface *surface, int x, int y, Uint32 pixel) {
-	Uint8 *target_pixel = (Uint8 *)(*surface).pixels 
-		+ y * (*surface).pitch
-		+ x * 4;
-	*(Uint32 *)target_pixel = pixel;
-}
-
-void draw_circle(SDL_Surface *surface, int n_cx, int n_cy, int radius, 
-		Uint32 pixel) {
-	double error = (double)-radius;
-	double x = (double)radius - 0.5;
-	double y = (double)0.5;
-	double cx = n_cx - 0.5;
-	double cy = n_cy - 0.5;
-
-	while (x >= y) {
-
-		set_pixel (surface, (int)(cx + x), (int)(cy + y), pixel);
-		set_pixel (surface, (int)(cx + y), (int)(cy + x), pixel);
-
-		if (x != 0) {
-		set_pixel (surface, (int)(cx - x), (int)(cy + y), pixel);
-		set_pixel (surface, (int)(cx + y), (int)(cy - x), pixel);
-		}
-
-		if (y != 0) {
-		set_pixel (surface, (int)(cx + x), (int)(cy - y), pixel);
-		set_pixel (surface, (int)(cx - y), (int)(cy + x), pixel);
-		}
-
-		if (x != 0 && y != 0) {
-		set_pixel (surface, (int)(cx - x), (int)(cy - y), pixel);
-		set_pixel (surface, (int)(cx - y), (int)(cy - x), pixel);
-		}
-
-		error += y;
-		++y;
-		error += y;
-
-		if (error >= 0) {
-			--x;
-			error -= x;
-			error -= x;
-		}
-	}
-}
-
-
 int main (int argc, char* argv[]) {
 
 	static const int width = 1024;
