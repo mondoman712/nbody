87b3a49f9de61b10221635f1db21ba51a13a6ebc
mondoman712 <oommmmmmmo@googlemail.com>
2015-09-29 15:10:40 +0100

    removed commented out threading code, because apparently I dont need it

--- main.c
+++ main.c
@@ -69,14 +69,8 @@ int main (int argc, char* argv[]) {
 		return 1;
 	}
 
-//	pthread_t tid_sdl;
-
-//	pthread_create(&tid_sdl, NULL, SDL_main_loop, NULL);
-
 	scm_with_guile(&register_functions, NULL);
 	scm_shell(argc, argv);
-
-//	pthread_join(tid_sdl, NULL);
 
 	SDL_main_loop();
 
@@ -88,4 +82,3 @@ int main (int argc, char* argv[]) {
 
 	return 0;
 }
-
