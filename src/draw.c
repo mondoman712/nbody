84703dab400488d1b423787f3adbc4a4c48b0980
mondoman712 <oommmmmmmo@googlemail.com>
2015-09-29 23:41:46 +0100

    started writing draw circle function in new file

---
+++ src/draw.c
@@ -0,0 +1,17 @@
+#include "defs.h"
+
+int draw_circle(SDL_Renderer *rend, Sint16 x, Sint16 y, Sint16 rad,
+		Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
+
+	SDL_setRenderDrawColor(rend, r, g, b, a);
+
+	if (rad < 0) {
+		return -1;
+	} else if (rad == 1) {
+		SDL_RenderDrawPoint(rend, x, y);
+		return 0;
+	}
+
+	Sint16 countx = 0;
+	Sint16 county = rad;
+	Sint16 
