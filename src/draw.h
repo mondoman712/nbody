775b6f09053ccaadb31a503847804554e56a3a9c
mondoman712 <oommmmmmmo@googlemail.com>
2015-09-30 12:57:41 +0100

    also added draw.h

---
+++ src/draw.h
@@ -0,0 +1,9 @@
+#ifndef _draw_h
+#define _draw_h
+
+#include "defs.h"
+
+int draw_circle(SDL_Renderer *rend, Sint16 x, Sint16 y, Sint16 rad,
+		Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+
+#endif // _draw_h
