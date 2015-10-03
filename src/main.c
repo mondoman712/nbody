18b154c99f4738b2b0094478b894c798c55b09f9
mondoman712 <oommmmmmmo@googlemail.com>
2015-10-04 00:49:12 +0100

    .

--- src/main.c
+++ src/main.c
@@ -11,9 +11,10 @@
 void *SDL_main_loop(){
 	while (1) {
 		SDL_Event e;
-		if (SDL_PollEvent (&e))
+		if (SDL_PollEvent (&e)) {
 			if (e.type == SDL_QUIT)
 				break;
+		}
 
 //		SDL_RenderClear(renderer);
 		SDL_RenderPresent(renderer);
