49206ddfc2a1378ae832359e8c2a6e39bf27f85a
mondoman712 <oommmmmmmo@googlemail.com>
2015-10-09 17:31:52 +0100

    removed most of what i did, sort of startign over

--- src/body.h
+++
@@ -1,6 +0,0 @@
-#ifndef _body_h
-#define _body_h
-
-int add_body (vector pos, vector vel, int mass);
-
-#endif // _body_h
--- src/scheme_interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef _scheme_interface_h
-#define _scheme_interface_h
-
-// Function Definitions
-SCM clear_screen();
-SCM draw_square(int x, int y, int w, int h, int r, int g, int b, int a);
-void* register_functions(void* data);
-
-#endif // _scheme_interface_h
