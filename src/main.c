afbc0ce3e6953269dd2dbf92b097f7c81ba2f9ae
mondoman712 <oommmmmmmo@googlemail.com>
2016-02-07 21:48:17 +0000

    moved create shader to slibs

--- src/main.c
+++ src/main.c
@@ -34,6 +34,7 @@
 #include <libguile.h>
 
 #include "slibs/v_3dt.h"
+#include "slibs/g_lsl.h"
 
 #define G 6.67408e-11L
 #define PI 3.141592653589
@@ -60,62 +61,6 @@ typedef struct {
 	vec3 vel;
 	GLuint64 mass;
 } body;
-
-/*
- * Reads and compiles a .glsl shader file in the shaders folder, from just the
- * core of the filename (to use shaders/vs1.glsl, filename is just vsl)
- */
-GLuint create_shader (const GLenum shader_type, const char * filename)
-{
-	char _source[4096];
-	char dest[64];
-
-	/* Create full filename */
-	strcpy(dest, SHADER_DIR);
-	strcat(dest, filename);
-	strcat(dest, SHADER_EXT);
-
-	/* Open file */
-	FILE * s = fopen(dest, "r");
-	if (s == NULL) {
-		fprintf(stderr, "Could not open file %s\n", dest);
-		return 1;
-	}
-
-	/* Read all lines in file */
-	int i = 0;
-	char inc;
-	while (fscanf(s, "%c", &inc) > 0)
-		_source[i++] = inc;
-	
-	/* Close file */
-	fclose(s);
-
-	/* Add string terminator to file */
-	_source[i - 1] = '\0';
-
-	/* Change type of source */
-	const char * source = _source;
-
-	/* Compile Shader */
-	GLuint shader = glCreateShader(shader_type);
-	glShaderSource(shader, 1, &source, NULL);
-	glCompileShader(shader);
-
-	/* Check for and report errors */
-	GLint status;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE) {
-		char buff[512];
-		glGetShaderInfoLog(shader, 512, NULL, buff);
-		fprintf(stderr, "Shader %s failed to compile with error:\n", 
-				filename);
-		fprintf(stderr, "%s\n", buff);
-		fprintf(stderr, "\n");
-	}
-
-	return shader;
-}
 
 /* 
  * Reads obj file and deposits vertices into float array
